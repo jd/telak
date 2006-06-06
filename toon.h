42660551d47beee124fce4a6baeda50b2471e891
Julien Danjou <julien@danjou.info>
2006-06-06 18:22:57 +0000

    get ride of useless includes

--- toon.h
+++ toon.h
@@ -1,7 +1,4 @@
-#include <X11/Intrinsic.h>
 #include <X11/Xlib.h>
-#include <X11/Xos.h>
-#include <X11/extensions/shape.h>
 
 #define TOON_MESSAGE_LENGTH 128
 char toon_message[TOON_MESSAGE_LENGTH];
